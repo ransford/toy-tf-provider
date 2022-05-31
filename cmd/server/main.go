6b566206d12f1beccadb68afbd48029a278c500a
Ben Ransford <ben@ransford.org>
2022-05-30 18:05:45 -0700

    found the problem

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -7,7 +7,7 @@ import (
 	"strconv"
 )
 
-var barVal = 3
+var barVal = 13
 
 func getBar(w http.ResponseWriter, req *http.Request) {
 	log.Printf("got bar = %d\n", barVal)
@@ -15,10 +15,15 @@ func getBar(w http.ResponseWriter, req *http.Request) {
 }
 
 func putBar(w http.ResponseWriter, req *http.Request) {
-	_newFoo := req.FormValue("foo")
+	log.Println("in putBar")
+
+	// TODO: input is JSON, so decode JSON
+	_newBar := req.FormValue("bar")
+	log.Printf("new bar: %s", _newBar)
+
 	var newBar int
 	var err error
-	if newBar, err = strconv.Atoi(_newFoo); err != nil {
+	if newBar, err = strconv.Atoi(_newBar); err != nil {
 		w.WriteHeader(http.StatusBadRequest)
 		return
 	}
